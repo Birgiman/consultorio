ab8f242e1b1235f889e97dc99fc7f7547eafa639
Birgiman <eduardo.birgiman@outlook.com>
2023-08-02 11:59:06 -0300

    Adding new enviroment variables

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from './shared/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const frontEndURL = 'https://consultorio-alpha.vercel.app';
-
   app.enableCors({
-    origin: frontEndURL,
+    origin: [env.feDevURL, env.feProdURL],
     credentials: true,
   });
 
